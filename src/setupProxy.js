9febf69a5b4646eeef0f38eeac6e37ab79afa3f6
Gabriel Gitonga <gabworks51@gmail.com>
2022-12-28 16:07:37 +0300

    Fixed proxy issue

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = app => {
-    app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
+module.exports = function(app) {
+    app.use(createProxyMiddleware('/proxypath', { target: 'https://obscure-mesa-07404.herokuapp.com' }));
 };
